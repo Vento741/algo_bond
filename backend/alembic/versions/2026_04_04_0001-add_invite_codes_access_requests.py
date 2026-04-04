7350502ef0df9572051cd3ee37f39804c716da0d
Vento741 <vento741@mail.ru>
2026-04-04 16:44:13 +0300

    fix: correct alembic down_revision to chain from current head

--- backend/alembic/versions/2026_04_04_0001-add_invite_codes_access_requests.py
+++ backend/alembic/versions/2026_04_04_0001-add_invite_codes_access_requests.py
@@ -13,7 +13,7 @@ from sqlalchemy.dialects import postgresql
 
 # revision identifiers, used by Alembic.
 revision: str = "a1b2c3d4e5f6"
-down_revision: Union[str, None] = "ddcb3eec09b7"
+down_revision: Union[str, None] = "d17fc93d82f6"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
